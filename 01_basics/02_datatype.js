e89b9b534806050f4f19d481ea506576e961d6bb
Aviral-09 <aviral.ashu09@gmail.com>
2025-11-10 19:12:50 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+//alert("3+3")// we are using node js not browser
+
+ let name = "hitesh"
+ let age = 18
+ let isloggedin = false
+
+ //number=> 2 to power 53
+ //bigint => 
+//string => ""
+//bollean => true/false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+//object 
+console.log(typeof null);
