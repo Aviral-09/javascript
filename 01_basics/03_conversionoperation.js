83def4c0ecc34f37f0a26587812d2ee5638efaef
Aviral-09 <aviral.ashu09@gmail.com>
2025-11-10 20:52:43 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,55 @@
+let score = "33"
+
+//console.log(typeof score );
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score)//string
+//console.log(typeof valueInNumber);//number
+//console.log(valueInNumber);//NaN
+
+//"33" => 33
+//"22abc" => NaN
+// true => 1; false => 0
+
+// let isloggedin = 1
+
+// let bolleanisloggedin = Bollean(isloggedin)
+//console.log(bolleanisloggedin);
+
+// 1 =>true; 0 => false
+// ""=> false
+// "hitesh"=>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *******************************operations******************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 +str2
+console.log(str3);
+
+console.log("1" + 2);//12 
+console.log(2 + "1");//21
+console.log("1" +2 +2);//122
+console.log(1+2+"2");//32
+
+console.log(+true);//1
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+
+
+
